60dfb7e25e72c177bf907f45200e9c53c87d0df8
mrrevo55 <kwolny31@gmail.com>
2022-10-24 20:07:50 +0200

    Initial commit

---
+++ src/main/java/com/lastcivilization/equipmentwriteservice/EquipmentWriteServiceApplication.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.equipmentwriteservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EquipmentWriteServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EquipmentWriteServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lastcivilization/equipmentwriteservice/EquipmentWriteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lastcivilization.equipmentwriteservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EquipmentWriteServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
