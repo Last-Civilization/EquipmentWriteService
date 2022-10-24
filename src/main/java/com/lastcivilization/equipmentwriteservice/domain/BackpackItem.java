b727ea14c1ad63dfa40895a7a20b820ac217c1e9
mrrevo55 <kwolny31@gmail.com>
2022-10-24 20:21:37 +0200

    Implement model

---
+++ src/main/java/com/lastcivilization/equipmentwriteservice/domain/BackpackItem.java
@@ -0,0 +1,8 @@
+package com.lastcivilization.equipmentwriteservice.domain;
+
+class BackpackItem {
+
+    private Long id;
+    private Long itemId;
+    private int amount;
+}
--- src/main/java/com/lastcivilization/equipmentwriteservice/domain/Equipment.java
+++ src/main/java/com/lastcivilization/equipmentwriteservice/domain/Equipment.java
@@ -1,5 +1,15 @@
 package com.lastcivilization.equipmentwriteservice.domain;
 
+import java.util.List;
+
 class Equipment {
 
+    private Long id;
+    private Long helmet;
+    private Long armor;
+    private Long boots;
+    private Long pants;
+    private Long weapon;
+    private Long shield;
+    private List<BackpackItem> backpack;
 }
